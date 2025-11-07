1d38fa35c05a81b6f5fcf6d6925b5ea8b4a31863
Rabih Kahaleh <rabihkahaleh@hotmail.om>
2025-11-07 16:04:42 +0200

    commit3

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-       my count is   count is {count}
+       my count is mwhatever u want toxunt   count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
